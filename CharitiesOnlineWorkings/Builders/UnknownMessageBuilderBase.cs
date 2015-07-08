6c2847913ee61bb347056b50daf6da70fbb3e93f
Corin Ashwell <c.ashwell@comicrelief.com>
2015-07-08 10:06:43 +0100

    UnknownMessageBuilder for null return on MessageBuilderFactory

---
+++ CharitiesOnlineWorkings/Builders/UnknownMessageBuilderBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharitiesOnlineWorkings.Builders
+{
+    public class UnknownMessageBuilder : GovTalkMessageBuilderBase
+    {
+        public override void SetBody()
+        {
+            throw new NotImplementedException();
+        }
+        public override void SetEnvelopeVersion()
+        {
+            throw new NotImplementedException();
+        }
+        public override void SetGovTalkDetails()
+        {
+            throw new NotImplementedException();
+        }
+        public override void SetHeader()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
