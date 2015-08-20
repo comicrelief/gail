9b51e91906f0d830126dd1283ba9f21398d18981
Corin Ashwell <c.ashwell@comicrelief.com>
2015-08-20 16:46:02 +0100

    Added ErrorClasses

---
+++ CharitiesOnline/Models/GatewayErrors/ErrorClasses.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CharitiesOnline.Models
+{
+    public class GatewayError : IGatewayError
+    {
+        public int ErrorCode { get; set; }
+        public string ErrorDescription { get; set; }
+        public string ErrorRaisedBy { get; set; }
+        public string ErrorType { get; set; }
+        public string ErrorLocation { get; set; }
+       
+    }
+}
