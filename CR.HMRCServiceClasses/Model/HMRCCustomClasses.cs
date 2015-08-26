095f8ef7fde393f8ff21ca3b3931a22f1db9ae69
Corin Ashwell <c.ashwell@comicrelief.com>
2015-08-26 15:14:54 +0100

    Custom class for StatusReport from list response messages

---
+++ CR.HMRCServiceClasses/Model/HMRCCustomClasses.cs
@@ -0,0 +1,230 @@
+﻿using System.Xml.Serialization;
+
+namespace hmrcclasses
+{
+    //public partial class GovTalkMessageBody
+    //{
+    //    private GovTalkMessageBodyStatusReport statusReportField;
+    //    public GovTalkMessageBodyStatusReport StatusReport
+    //    {
+    //        get
+    //        {
+    //            return this.statusReportField;
+    //        }
+    //        set
+    //        {
+    //            this.statusReportField = value;
+    //        }
+    //    }
+    //}
+
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.govtalk.gov.uk/CM/envelope")]
+    [System.Xml.Serialization.XmlRootAttribute("StatusReport", Namespace = "http://www.govtalk.gov.uk/CM/envelope", IsNullable = false)]    
+    public partial class GovTalkMessageBodyStatusReport
+    {
+        private string senderIDField;
+
+        private string startTimeStampField;
+
+        private string endTimeStampField;
+
+        private GovTalkMessageBodyStatusReportStatusRecord[] statusRecordField;
+
+        /// <remarks/>
+        public string SenderID
+        {
+            get
+            {
+                return this.senderIDField;
+            }
+            set
+            {
+                this.senderIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string StartTimeStamp
+        {
+            get
+            {
+                return this.startTimeStampField;
+            }
+            set
+            {
+                this.startTimeStampField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string EndTimeStamp
+        {
+            get
+            {
+                return this.endTimeStampField;
+            }
+            set
+            {
+                this.endTimeStampField = value;
+            }
+        }
+
+        /// <remarks/>
+        [System.Xml.Serialization.XmlElementAttribute("StatusRecord")]
+        public GovTalkMessageBodyStatusReportStatusRecord[] StatusRecord
+        {
+            get
+            {
+                return this.statusRecordField;
+            }
+            set
+            {
+                this.statusRecordField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.govtalk.gov.uk/CM/envelope")]
+    public partial class GovTalkMessageBodyStatusReportStatusRecord
+    {
+
+        private string timeStampField;
+
+        private string correlationIDField;
+
+        private string transactionIDField;
+
+        private string statusField;
+
+        /// <remarks/>
+        public string TimeStamp
+        {
+            get
+            {
+                return this.timeStampField;
+            }
+            set
+            {
+                this.timeStampField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string CorrelationID
+        {
+            get
+            {
+                return this.correlationIDField;
+            }
+            set
+            {
+                this.correlationIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string TransactionID
+        {
+            get
+            {
+                return this.transactionIDField;
+            }
+            set
+            {
+                this.transactionIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Status
+        {
+            get
+            {
+                return this.statusField;
+            }
+            set
+            {
+                this.statusField = value;
+            }
+        }
+    }
+
+    /// <remarks/>
+    [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+    [System.SerializableAttribute()]
+    [System.Diagnostics.DebuggerStepThroughAttribute()]
+    [System.ComponentModel.DesignerCategoryAttribute("code")]
+    [System.Xml.Serialization.XmlTypeAttribute(AnonymousType = true, Namespace = "http://www.govtalk.gov.uk/CM/envelope")]
+    public partial class GovTalkMessageBodyStatusRecord
+    {
+
+        private string timeStampField;
+
+        private string correlationIDField;
+
+        private string transactionIDField;
+
+        private string statusField;
+
+        /// <remarks/>
+        public string TimeStamp
+        {
+            get
+            {
+                return this.timeStampField;
+            }
+            set
+            {
+                this.timeStampField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string CorrelationID
+        {
+            get
+            {
+                return this.correlationIDField;
+            }
+            set
+            {
+                this.correlationIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string TransactionID
+        {
+            get
+            {
+                return this.transactionIDField;
+            }
+            set
+            {
+                this.transactionIDField = value;
+            }
+        }
+
+        /// <remarks/>
+        public string Status
+        {
+            get
+            {
+                return this.statusField;
+            }
+            set
+            {
+                this.statusField = value;
+            }
+        }
+    }
+}
