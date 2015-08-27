9ba71bef4a3b7ec81837e5755c06f9a69231af13
Corin Ashwell <c.ashwell@comicrelief.com>
2015-08-27 18:01:08 +0100

    Added correlationId

--- CharitiesOnline/Models/GatewayErrors/GatewayError.cs
+++ CharitiesOnline/Models/GatewayErrors/GatewayError.cs
@@ -4,11 +4,13 @@ namespace CharitiesOnline.Models
 {
     public class GatewayError : IGatewayError
     {
-        public int ErrorCode { get; set; }
-        public string ErrorDescription { get; set; }
+        public string CorrelationId { get; set; }
+        public int ErrorNumber { get; set; }
+        public string ErrorText { get; set; }
         public string ErrorRaisedBy { get; set; }
         public string ErrorType { get; set; }
         public string ErrorLocation { get; set; }
+        public string ErrrorApplicationMessage { get; set; }
        
     }
 }
