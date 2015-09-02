4fbb78ccb7ca138e59127e2ed68f4c370685f53a
Corin Ashwell <c.ashwell@comicrelief.com>
2015-09-02 17:41:32 +0100

    This is just some toy helpers for my own development and debugging purposes.

---
+++ CR.CO.Demo/LocalHelp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace CR.CO.Demo
+{
+    public class LocalHelp
+    {
+        public static void ConsolePrintDataTable(System.Data.DataTable inputTable)
+        {
+            foreach (DataColumn column in inputTable.Columns)
+                Console.Write("\t{0}", column.ColumnName);
+
+            Console.WriteLine("");
+            foreach (DataRow row in inputTable.Rows)
+            {
+                foreach (DataColumn column in inputTable.Columns)
+                    Console.Write("\t{0}", row[column]);
+                Console.WriteLine("");
+            }
+        }
+    }
+}
