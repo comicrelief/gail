7d5ab38be00857d6ae460ce25ff941214d64d3eb
Corin Ashwell <c.ashwell@comicrelief.com>
2015-09-18 20:12:22 +0100

    Don't know why EnptyBodyBuilder was missing. Can't figure out how to exclude that zip file.

---
+++ CharitiesOnline/MessageBuilders/EmptyBodyBuilder.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+using hmrcclasses;
+using CR.Infrastructure.Logging;
+using CharitiesOnline.Helpers;
+
+namespace CharitiesOnline.MessageBuilders
+{
+    public class EmptyBodyBuilder : BodyBuilderBase
+    {
+        private ILoggingService _loggingService;
+
+        public EmptyBodyBuilder(ILoggingService loggingService)
+        {
+            _loggingService = loggingService;
+        }
+        public void CreateBody()
+        {
+            InitialiseBody(_loggingService);
+            BuildBody();
+        }
+
+        public override void BuildBody()
+        {
+        }
+
+        public override void AddBodyElements()
+        {
+        }
+    }
+}
